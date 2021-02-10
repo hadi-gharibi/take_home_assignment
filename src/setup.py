3ca4bc492cca6ce18a8d53e743472d379aeae1b6
Hadi Gharibi <hady.gharibi@gmail.com>
2021-02-10 14:59:08 +0100

    add find_package

--- src/setup.py
+++ src/setup.py
@@ -1,20 +1,24 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 setup(
-    name="example",
+    name="mypack",
     version="0.0.1",
     # Author details
     author="<Add your name>",
     author_email="<Add your email address>",
     # Choose your license
     license="All Rights Reserved",
-    package_dir={"": "main"},
+    #package_dir={"": "main"},
+    packages = find_packages(include='tests'),
     entry_points={"console_scripts": ["db_import=assignment.main:main"]},
     install_requires=[
         "psycopg2==2.8.6",
+        "python-dateutil",
+        "SQLAlchemy"
     ],
     tests_require=[
         "pytest",
